b03d439cede6f120261ec982ddd3eac22b19926e
martybalaban <marty@teltech.co>
2022-03-03 14:17:25 -0500

    Adding openpgp import

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"github.com/bitrise-io/go-utils/retry"
 	"github.com/bitrise-tools/go-steputils/stepconf"
     "github.com/google/go-github/github"
+    "golang.org/x/crypto/openpgp"
+    "golang.org/x/crypto/openpgp/armor"
 )
 
 // formats:
